0fd239846b525045bee0b22db44eb3741bc47d34
Emmanuel Perez Perez <emmanuelperezp@gmail.com>
2018-03-01 21:35:52 -0600

    loading

--- sketch.js
+++ sketch.js
@@ -3,46 +3,49 @@ var img;
 var movers = [];
 var numberofobjects;
 
+function preload() {
+  img = loadImage('caquita.png');
+}
+
 function playSound() {
   movers.push(new Mover());
-  numberofobjects+=1;
+  numberofobjects += 1;
   console.log("cantidad de caquitas: " + numberofobjects.toString())
   var audio;
   var num = random(1);
   if (num < 0.5) {
-    audio = new Audio('fart0.mp3');
-  }
-  else if (num < 0.7) {
-    audio = new Audio('fart1.mp3');
+    audio0 = new Audio('fart0.mp3');
+    audio0.play();
+    console.log("audio0");
   }
   else if (num < 0.8){
-    var audio = new Audio('fart2.mp3');
+    audio2 = new Audio('fart2.mp3');
+    audio2.play();
+    console.log("audio2");
   }
   else if (num < 0.9){
-    var audio = new Audio('fart3.mp3');
+    audio3 = new Audio('fart3.mp3');
+    audio3.play();
+    console.log("audio3");
   }
   else{
-    var audio = new Audio('fart4.mp3');
+    audio4 = new Audio('fart4.mp3');
+    audio4.play();
+    console.log("audio4");
   }
-  audio.play();
-}
-
-function preload() {
-  img = loadImage('caquita.png');
 }
 
 function setup() {
   var canvas = createCanvas(windowWidth, windowHeight);
 
-  img = loadImage('caquita.png');
   // Move the canvas so it's inside our <div id="sketch-holder">.
   canvas.parent('p5-container');
 
   if(/Mobi/i.test(navigator.userAgent) || /Android/i.test(navigator.userAgent)){
-    numberofobjects = 10;
+    numberofobjects = 5;
   }
   else{
-    numberofobjects = 30;
+    numberofobjects = 5;
   }
 
   for (var i = 0; i < numberofobjects; i++) {
